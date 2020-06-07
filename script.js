922cffc09cf72342118034fd54ca39d401d49f26
ergunirem <iremthedeveloper@gmail.com>
2020-06-07 20:07:32 +0200

    deleted all functions except the operate function

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// VARIABLES
+let result = 0;
+let operation = 'on'
+let calculationList = [];
+document.getElementById("display").value = result
+
+// FUNCTIONS
+
 function operate(operator, arg1, arg2) {
     let functions = {
-        add: function (a, b){
+        '+': function (a, b){
             return a+b;
         },
-        subtract: function(a, b){
+        '-': function(a, b){
             return a-b;
         },
-        multiply: function (a, b){
+        '*': function (a, b){
             return a*b;
         },
-        divide: function(a, b) {
+        '/': function(a, b) {
             return a / b;
         }
     };
@@ -17,30 +25,4 @@ function operate(operator, arg1, arg2) {
     return functions[operator](arg1, arg2);
 }
 
-let result = 0;
-let operation = 'start'
-document.getElementById("display").value = result
 
-function numberClick(number) {
-    if (operation == 'start') {
-        result = number;
-        document.getElementById("display").value = result
-        console.log(result);
-    } else {
-        document.getElementById("display").value = number;
-        result = operate(operation, result, number);
-        console.log(result);
-    }
-}
-
-function operationClick(argument) {
-    //Displays the result of the previous calculation before a number for a new calculation is clicked
-    document.getElementById("display").value = result
-
-    //Saves the operator name to be used in operate() function
-    operation = argument;
-}
-
-function solve() {
-    document.getElementById("display").value = result;
-}
